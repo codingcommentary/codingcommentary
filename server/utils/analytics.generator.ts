f323f82d71b4df1b272c96f8e3de3783387c708a
Manishkumar Jangid <manishkumar76t@gmail.com>
2024-06-30 19:57:08 -0400

    Monthly data collection

--- server/utils/analytics.generator.ts
+++ server/utils/analytics.generator.ts
@@ -10,6 +10,14 @@ export async function generateLast12MonthsData<T extends Document>(
 ): Promise<{ last12Months: MonthData[] }> {
     const last12Months: MonthData[] = [];
     const currentDate = new Date();
-    currentDate.setDate(currentDate.getDate() + 1)
+    currentDate.setDate(currentDate.getDate() + 1);
+
+    for (let i = 12; i >=0; i--) {
+        const endDate = new Date (
+            currentDate.getFullYear(),
+            currentDate.getMonth(),
+            currentDate.getDate() - i *28
+        )
+    };
 
 }   
