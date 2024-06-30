5a770ff47ba0730cc10364b0b978c6ae3748600b
Manishkumar Jangid <manishkumar76t@gmail.com>
2024-06-30 19:50:41 -0400

    Month Array

---
+++ server/utils/analytics.generator.ts
@@ -0,0 +1,15 @@
+import { Document, Model } from "mongoose";
+
+interface MonthData {
+    month: string;
+    count: number;
+}
+
+export async function generateLast12MonthsData<T extends Document>(
+    model: Model<T>
+): Promise<{ last12Months: MonthData[] }> {
+    const last12Months: MonthData[] = [];
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + 1)
+
+}   
