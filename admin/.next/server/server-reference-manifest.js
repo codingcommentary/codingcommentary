f8853b07dc0bee5b501024f402f1c7f21938dffe
Manishkumar Jangid <manishkumar76t@gmail.com>
2024-07-06 23:13:10 -0400

    fd

--- admin/.next/server/server-reference-manifest.js
+++ admin/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"fX9/sUCacJsYzQvenKoI4/I9yl3E+GtSJKDxr6/nm/I=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"J250MYVRvNAaowF3lafYxiLjzi3YWpM6f6Ox5ynFvtg=\"\n}"
