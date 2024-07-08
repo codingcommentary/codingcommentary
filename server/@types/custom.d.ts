19d5021b8b82f8a20621ae1197f527693bd7b729
Mehul Kansal <mehulkansal99@gmail.com>
2024-07-08 19:46:00 -0400

    Added course enrollment functionalities for paid courses

--- server/@types/custom.d.ts
+++ server/@types/custom.d.ts
@@ -1,10 +1,29 @@
 import { Request } from "express";
 import { IUser } from "../models/user.model";
+import { ICourse } from "../models/course.model";
 
 declare global {
   namespace Express {
     interface Request {
       user?: IUser;
     }
+
+    interface IData {
+      courseId: string;
+      userId?: string;
+      payment_info?: {
+        id?: string;
+        [key: string]: any;
+      };
+    }
+  }
+
+  interface IMailData {
+    order: {
+      _id: string;
+      name: string;
+      price: number;
+      date: string;
+    };
   }
 }
---
+++ server/controllers/order.controller.ts
@@ -0,0 +1,118 @@
+/// <reference path="../@types/custom.d.ts" />
+
+import { NextFunction, Request, Response } from "express";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import ErrorHandler from "../utils/ErrorHandler";
+import { IOrder } from "../models/order.Model";
+import userModel from "../models/user.model";
+import CourseModel, { ICourse } from "../models/course.model";
+import path from "path";
+import ejs from "ejs";
+import sendMail from "../utils/sendMail";
+import NotificationModel from "../models/notification.Model";
+import { getAllOrdersService, newOrder } from "../services/order.service";
+import { redis } from "../utils/redis";
+import { AnyArray } from "mongoose";
+import { IMailData } from "../@types/types";
+require("dotenv").config();
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+// create order
+export const createOrder = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { courseId, payment_info } = req.body as IOrder;
+
+      // if (payment_info) {
+      //   if ("id" in payment_info) {
+      //     const paymentIntentId = payment_info.id;
+      //     const paymentIntent = await stripe.paymentIntents.retrieve(
+      //       paymentIntentId
+      //     );
+
+      //     if (paymentIntent.status !== "succeeded") {
+      //       return next(new ErrorHandler("Payment not authorized!", 400));
+      //     }
+      //   }
+      // }
+
+      const user = await userModel.findById(req.user?._id);
+
+      const courseExistInUser = user?.courses.some(
+        (course: any) => course._id.toString() === courseId
+      );
+
+      if (courseExistInUser) {
+        return next(
+          new ErrorHandler("You have already purchased this course", 400)
+        );
+      }
+
+      const course: ICourse | null = await CourseModel.findById(courseId);
+
+      if (!course) {
+        return next(new ErrorHandler("Course not found", 404));
+      }
+
+      const data: any = {
+        courseId: course._id,
+        userId: user?._id,
+        payment_info,
+      };
+
+      const courseIdNew = course?._id as string;
+
+      const mailData: IMailData = {
+        order: {
+          _id: courseIdNew.toString().slice(0, 6),
+          name: course.name,
+          price: course.price,
+          date: new Date().toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          }),
+        },
+      };
+
+      const html = await ejs.renderFile(
+        path.join(__dirname, "../mails/order-confirmation.ejs"),
+        { order: mailData }
+      );
+
+      try {
+        if (user) {
+          await sendMail({
+            email: user.email,
+            subject: "Order Confirmation",
+            template: "order-confirmation.ejs",
+            data: mailData,
+          });
+        }
+      } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+
+      user?.courses.push(course?.id);
+
+      const userId = req.user?._id as string;
+      await redis.set(userId, JSON.stringify(user));
+
+      await user?.save();
+
+      await NotificationModel.create({
+        user: user?._id,
+        title: "New Order",
+        message: `You have a new order from ${course?.name}`,
+      });
+
+      course.purchased += 1;
+
+      await course.save();
+
+      newOrder(data, res, next);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
